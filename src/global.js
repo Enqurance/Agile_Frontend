f5869606ef0be43c61b2ea6a41edee5dfa045639
徐凯 <20373398@buaa.edu.cn>
2023-04-09 15:44:49 +0800

    [style](global): 改变global.js的导出格式

--- src/global.js
+++ src/global.js
@@ -3,4 +3,19 @@ let global_token = ''
 // true : 管理员
 let global_type = false
 
-export default {global_user_id, global_token, global_type}
+let pin_type_dic = {
+    1: "教室",
+    2: "食堂",
+    3: "宿舍",
+    4: "办公",
+    5: "运动",
+    6: "超市",
+    7: "学习",
+}
+
+function get_pin_type(pin_type_id) {
+    // 如果不存在id，则输出""
+    return pin_type_dic[pin_type_id]
+}
+
+export default {global_user_id, global_token, global_type, get_pin_type}
