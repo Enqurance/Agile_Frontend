3b9a2f621c46f70cdbb0fe97399b34002bfdb1b6
pangrj <pangrj@buaa.edu.cn>
2023-04-16 10:40:19 +0800

    [feat](路由配置)：完成了路由配置与路由守卫

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,35 @@
-import HelloWorld from '../components/HelloWorld.vue'
-
+import InforPage from '../components/InforPage.vue'
+import login from '../components/login.vue'
+import register from '../components/register.vue'
+import MapHomePage from '../components/MapHomePage.vue'
 
 const routes = [
     {
         name: 'home',
         path: '/',
-        component: HelloWorld 
+        component: MapHomePage
     },
+    { 
+        name: 'InforPage',
+        path: '/InforPage', 
+        query:{} ,
+        component: InforPage,
+    },
+    {
+        name: 'login',
+        path: '/login',
+        component: login,
+    },
+    {
+        name: 'register',
+        path: '/register',
+        component: register,
+    },
+    {
+        name: 'MapHomePage',
+        path: '/MapHomePage',
+        component: MapHomePage,
+    }
 ]
 
 export default routes
