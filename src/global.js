041d495b4945eceadab01e892e0b7f9ee03d3759
徐凯 <20373398@buaa.edu.cn>
2023-04-17 23:00:39 +0800

    变更

--- src/global.js
+++ src/global.js
@@ -3,19 +3,4 @@ let global_token = ''
 // true : 管理员
 let global_type = false
 
-let pin_type_dic = {
-    1: "教室",
-    2: "食堂",
-    3: "宿舍",
-    4: "办公",
-    5: "运动",
-    6: "超市",
-    7: "学习",
-}
-
-function get_pin_type(pin_type_id) {
-    // 如果不存在id，则输出""
-    return pin_type_dic[pin_type_id]
-}
-
-export default {global_user_id, global_token, global_type, get_pin_type}
+export default {global_user_id, global_token, global_type}
