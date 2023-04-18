a35132a36b75a3300c4335b520f859edcf6aa243
徐凯 <20373398@buaa.edu.cn>
2023-04-18 11:16:21 +0800

    [fix](MapHomepage): help! and close issue

--- src/components/PlaceSearch.vue
+++ src/components/PlaceSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="is_display" style="height: 100%">
-        <el-row style="margin-bottom: 0.5%">
+        <el-row style="padding-bottom: 0.5%">
             <el-col :span="24">
                 <el-input v-model="search_content" placeholder="请输入查找信息" clearable maxlength="24"
                           @blur="open_sel=false" @click="search_for_content()" @input="search_for_content()"
@@ -13,21 +13,22 @@
             </el-col>
         </el-row>
 
-        <div v-if="open_sel && search_result_num > 5" class="result_div" style="height: 50%">
-            <el-scrollbar>
-                <div v-for="item in search_result" :key="item.id" style="margin: 0.5% 0.5%; padding: 2% 0"
+        <div v-if="open_sel && search_result_num > 5" class="result_div">
+            <el-scrollbar style="height: 150px">
+                <div v-for="item in search_result" :key="item.id"
+                     style="padding: 2% 0"
                      :class="{result_mouseover: mouseover_p_id === item.id}"
                      @mouseenter="mouseover_p_id = item.id" @mouseleave="mouseover_p_id = -1"
                      @click="change_show_id(item.id)">
-                    <div style="float: left; margin-left: 4%">
+                    <div style="float: left; padding-left: 4%">
                         <span class="iconfont icon-sousuo"> </span>
                     </div>
 
-                    <div style="float: left; margin-left: 5%;" class="max_length">
+                    <div style="float: left; padding-left: 5%;" class="max_length">
                         <span>{{ item.name }}</span>
                     </div>
 
-                    <div style="float: left; margin-left: 5%">
+                    <div style="float: left; padding-left: 5%">
                         <span style="color: cornflowerblue">{{ _get_pin_type(item.type) }}</span>
                     </div>
                     <div style="clear: both"></div>
@@ -35,22 +36,24 @@
             </el-scrollbar>
         </div>
         <div v-else-if="open_sel && search_result_num > 0 && search_result_num <= 5" class="result_div">
-            <div v-for="item in search_result" :key="item.id" style="margin: 0.5% 0.5%; padding: 2% 0"
+            <div v-for="item in search_result" :key="item.id"
+                 style="padding: 2% 0;height: 10px"
                  :class="{result_mouseover: mouseover_p_id === item.id}"
                  @mouseenter="mouseover_p_id = item.id" @mouseleave="mouseover_p_id = -1"
-                 @click="change_show_id(item.id)">
-                <div style="float: left; margin-left: 4%">
-                    <span class="iconfont icon-sousuo"> </span>
-                </div>
+                 @click=" change_show_id(item.id)">
+                {{item.id}}
+<!--                <div style="float: left; padding-left: 4%">-->
+<!--                    <span class="iconfont icon-sousuo"> </span>-->
+<!--                </div>-->
 
-                <div style="float: left; margin-left: 5%;" class="max_length">
-                    <span>{{ item.name }}</span>
-                </div>
+<!--                <div style="float: left; padding-left: 5%;" class="max_length">-->
+<!--                    <span>{{ item.name }}</span>-->
+<!--                </div>-->
 
-                <div style="float: left; margin-left: 5%">
-                    <span style="color: cornflowerblue">{{ _get_pin_type(item.type) }}</span>
-                </div>
-                <div style="clear: both"></div>
+<!--                <div style="float: left; padding-left: 5%">-->
+<!--                    <span style="color: cornflowerblue">{{ _get_pin_type(item.type) }}</span>-->
+<!--                </div>-->
+<!--                <div style="clear: both"></div>-->
             </div>
         </div>
         <div v-else-if="open_sel" class="result_div">
@@ -118,6 +121,7 @@ export default {
                     'token': that.$cookies.get('user_token')
                 }
             }).then((res) => {
+                console.log(res)
                 that.search_result = res.data.data.search_result_list
                 that.show_p_ip = res.data.data.max_suit_p_id
                 that.open_sel = true
@@ -138,6 +142,7 @@ export default {
             }
         },
         change_show_id(p_id) {
+            console.log("show_p_id: " + p_id)
             this.show_p_id = p_id
             this.sub_p_id(p_id)
         },
@@ -163,8 +168,9 @@ export default {
 <style scoped>
 
 .result_div {
-    border: 2px solid #f0f0f0;
-    border-radius: 3px;
+    /*border: 2px solid #f0f0f0;*/
+    border-radius: 0 0 10px 10px;
+    background: white;
 }
 
 .max_length {
@@ -175,7 +181,7 @@ export default {
 }
 
 .result_mouseover {
-    background: rgb(230, 230, 230);
+    background: #f0f0f0;
     cursor: pointer;
 }
 
