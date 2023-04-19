edf000dddd1d9da2c8e07e313df7696f3cd8014d
wzy2001wzy <864294516@qq.com>
2023-04-19 17:11:23 +0800

    [feat]():feedback button

---
+++ src/components/FeedbackButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-button @click="fk_dialogFormVisible = true">
+    123123
+  </el-button>
+  <el-dialog v-model="fk_dialogFormVisible" title="错误反馈" width="80%">
+    <el-form :data="formData" height="250" style="width: 100%">
+      <el-form-item label="标题">
+        <el-input v-model="formData.title"/>
+      </el-form-item>
+      <el-form-item label="详细信息">
+        <el-input v-model="formData.detail"/>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="fk_dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="fankui()">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+import global from "@/global";
+
+export default {
+  name: "FeedbackButton",
+  data() {
+    return {
+      fk_dialogFormVisible: false,
+      formData: [],
+    }
+  },
+  methods:
+      {
+        fankui() {
+          let that = this
+          that.$axios.post('/map/pin/feedback', {
+            id: 0,
+            title: that.formData.title,
+            position: that.formData.detail,
+          }, {
+            headers: {
+              'token': global.global_token
+            }
+          })
+              .then(response => {
+                console.log(response);
+                this.dialogVisible = false;
+              })
+              .catch(error => {
+                console.error(error);
+              });
+        }
+      },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,125 +1,126 @@
 <template>
-    <div class="login-container">
-        <div class="logo-container">
-            <img src="https://s2.loli.net/2023/04/11/b8pzQZCJNeoSmyE.jpg" alt="logo" style="width: 100%; height: auto;">
-        </div>
-        <div class="form-container">
-            <el-form ref="loginForm" :model="loginForm" class="login-form">
-                <el-form-item prop="email">
-                    <el-input v-model="loginForm.email" placeholder="请输入用户邮箱" @keyup.enter="login"></el-input>
-                </el-form-item>
-                <el-form-item prop="password">
-                    <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"
-                              @keyup.enter="login"></el-input>
-                </el-form-item>
-
-                <el-form-item>
-                    <div class="button-container">
-                        <el-button @click="goToRegister">注册</el-button>
-                        <el-button type="primary" @click="login">登录</el-button>
-                    </div>
-                </el-form-item>
-            </el-form>
-        </div>
+  <div class="login-container">
+    <div class="logo-container">
+      <img src="https://s2.loli.net/2023/04/11/b8pzQZCJNeoSmyE.jpg" alt="logo" style="width: 100%; height: auto;">
     </div>
+    <div class="form-container">
+      <el-form ref="loginForm" :model="loginForm" class="login-form">
+        <el-form-item prop="email">
+          <el-input v-model="loginForm.email" placeholder="请输入用户邮箱" @keyup.enter="login"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"
+                    @keyup.enter="login"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <div class="button-container">
+            <el-button @click="goToRegister">注册</el-button>
+            <el-button type="primary" @click="login">登录</el-button>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
 import global from '@/global'
+import FanKuibutton from "@/components/FeedbackButton";
 
 export default {
-    name: "LoginPage",
-    data() {
-        return {
-            loginForm: {
-                email: "",
-                password: "",
-            },
-            loginTitle: {
-                email: "用户邮箱",
-                password: "密码",
-            },
-        };
+  name: "LoginPage",
+  components: {FanKuibutton},
+  data() {
+    return {
+      loginForm: {
+        email: "",
+        password: "",
+      },
+      loginTitle: {
+        email: "用户邮箱",
+        password: "密码",
+      },
+    };
+  },
+
+  methods: {
+    login() {
+      for (var i in this.loginForm) {
+        if (this.loginForm[i].trim().length == 0)
+          return this.$message.error(this.loginTitle[i] + "不能为空")
+      }
+
+      let that = this
+      that.$axios.post('/auth/login', {
+        email: that.loginForm.email,
+        password: that.loginForm.password
+      }).then((res) => {
+        if (res.data.code === 200) {
+          global.global_token = res.data.data.token
+          global.global_type = res.data.data.type
+          that.$router.push({path: '/home'})
+        } else {
+          alert(res.data.message)
+        }
+      }).catch((res) => console.log(res))
     },
 
-    methods: {
-        login() {
-            for (var i in this.loginForm) {
-                if (this.loginForm[i].trim().length == 0)
-                    return this.$message.error(this.loginTitle[i] + "不能为空")
-            }
-
-            let that = this
-            that.$axios.post('/auth/login', {
-                email: that.loginForm.email,
-                password: that.loginForm.password
-            }).then((res) => {
-                if (res.data.code === 200) {
-                    global.global_token = res.data.data.token
-                    global.global_type = res.data.data.type
-                    that.$router.push({path: '/home'})
-                }
-                else {
-                    alert(res.data.message)
-                }
-            }).catch((res) => console.log(res))
-        },
-
-        goToRegister() {
-            this.$router.push({path: "/register"});
-        },
+    goToRegister() {
+      this.$router.push({path: "/register"});
     },
+  },
 };
 </script>
 
 <style>
 .login-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 
 .logo-container {
-    width: 100px;
-    height: 100px;
-    background: #ccc;
-    margin-bottom: 20px;
-    /* 添加20像素的下边距 */
-    text-align: center;
+  width: 100px;
+  height: 100px;
+  background: #ccc;
+  margin-bottom: 20px;
+  /* 添加20像素的下边距 */
+  text-align: center;
 }
 
 
 .form-container {
-    display: flex;
-    /* 添加Flex属性 */
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  /* 添加Flex属性 */
+  justify-content: center;
+  align-items: center;
 }
 
 .login-form {
-    flex: 1;
-    /* 添加Flex属性 */
+  flex: 1;
+  /* 添加Flex属性 */
 }
 
 .el-form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .el-form-item {
-    width: 100%;
-    text-align: center;
+  width: 100%;
+  text-align: center;
 }
 
 .button-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 400px;
-    margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 400px;
+  margin: 0 auto;
 }
 </style>
 
