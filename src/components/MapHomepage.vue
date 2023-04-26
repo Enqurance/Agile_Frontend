d8cb6c060102872f6080e345ed15bf53b9aebd9d
徐凯 <20373398@buaa.edu.cn>
2023-04-26 16:40:27 +0800

    [feat](MapHomepage): 修改移动端界面

--- src/components/MapHomepage.vue
+++ src/components/MapHomepage.vue
@@ -1,13 +1,15 @@
 <template>
     <PCMapHomepage v-if="this.$device==='pc'"/>
+    <MobileMapHomepage v-else />
 </template>
 
 <script>
 import PCMapHomepage from "@/components/pc/PCMapHomepage.vue";
+import MobileMapHomepage from "@/components/mobile/MobileMapHomepage.vue";
 
 export default {
     name: "MapHomepage",
-    components: {PCMapHomepage}
+    components: {MobileMapHomepage, PCMapHomepage}
 }
 </script>
 
