52df4e028e56693cfaddc0bfe620d354de6d4480
徐凯 <20373398@buaa.edu.cn>
2023-05-06 22:00:28 +0800

    [feat](PinInfo): 公共pin申请

--- src/global.js
+++ src/global.js
@@ -18,6 +18,18 @@ let pin_color_dic = {
     7: "pink"
 }
 
+let pin_state_dic = {
+    0: "可申请",
+    1: "待审批",
+    2: "申请冷却",
+}
+
+let pin_state_color_dic = {
+    0: "springGreen",
+    1: "yellow",
+    // 2: "red",
+}
+
 function get_pin_type(pin_type_id) {
     // 如果不存在id，则输出""
     return pin_type_dic[pin_type_id]
@@ -28,4 +40,14 @@ function get_pin_color(pin_type_id) {
     return pin_color_dic[pin_type_id]
 }
 
-export default {get_pin_type, get_pin_color}
+function get_pin_state(pin_state_id) {
+    // 如果不存在id，则输出""
+    return pin_state_dic[pin_state_id]
+}
+
+function get_pin_state_color(pin_state_id) {
+    // 如果不存在id，则输出""
+    return pin_state_color_dic[pin_state_id]
+}
+
+export default {get_pin_type, get_pin_color, get_pin_state, get_pin_state_color}
