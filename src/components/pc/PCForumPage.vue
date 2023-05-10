7889c7e64f2f7f3ebb46a6e8cc0194040fa22079
Your Name <you@example.com>
2023-05-10 20:48:01 +0800

    [feat]():新增帖子组件:

--- src/components/pc/PCForumPage.vue
+++ src/components/pc/PCForumPage.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <PageHeader style="position: fixed; top: 0; width: 100%; z-index: 3; background: black; opacity: 0.3" />
-        <p>这是论坛页面</p>
+        <p style="top: 20%">这是论坛页面</p>
+
+        <el-button @click="showNewPostDialog">显示固定组件</el-button>
+        <new-post ref="child"></new-post>
     </div>
 
 </template>
@@ -9,13 +12,20 @@
 
 <script>
 import PageHeader from "@/components/pc/PCPageHeader.vue";
+import NewPost from "../sub_components/NewPost.vue";
 
 export default {
     name: "PCMapHomepage",
 
     components: {
         PageHeader,
+        NewPost,
     },
 
+    methods: {
+        showNewPostDialog() {
+            this.$refs.child.dialogVisible = true
+        }
+    }
 }  
 </script>
