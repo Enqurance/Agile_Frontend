1e79a4571306a1dac649484bcdb745587659dca4
Enqurance <20373980@buaa.edu.cn>
2023-05-12 10:04:03 +0800

    (fix)[App.vue]:Retrun to router 121212121212121212121212

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
+<<<<<<< HEAD
     <router-view/>
+=======
+   <router-view/>
+  <!-- <MyMessage></MyMessage> -->
+>>>>>>> 203d784 ((fix)[App.vue]:Retrun to router)
 </template>
 
 <script>
 
+<<<<<<< HEAD
 import MyMessage from "@/components/MyMessage";
 import MyPost from "@/components/MyPost";
 import MyComment from "@/components/MyComment";
 export default {
     name: 'App',
     components: {},
+=======
+export default {
+    name: 'App',
+>>>>>>> 203d784 ((fix)[App.vue]:Retrun to router)
     provide() {
         return {
             reload: this.reload
