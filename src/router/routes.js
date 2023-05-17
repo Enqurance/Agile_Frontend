dad14a29c0ae7fbfbaeea119bd9561cd19f9910c
徐凯 <20373398@buaa.edu.cn>
2023-05-17 10:09:53 +0800

    [chore](): 更改文件路径

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,9 @@ import InforPage from "@/components/InforPage.vue";
 import MapHomepage from '../components/MapHomepage.vue'
 import LoginPage from "@/components/LoginPage.vue";
 import RegisterPage from "@/components/RegisterPage.vue";
-import MyPost from "@/components/MyPost";
-import MyComment from "@/components/MyComment";
-import MyMessage from "@/components/MyMessage";
+import MyPost from "@/components/sub_components/infopage_sub_components/MyPost.vue";
+import MyComment from "@/components/sub_components/infopage_sub_components/MyComment.vue";
+import MyMessage from "@/components/sub_components/infopage_sub_components/MyMessage.vue";
 import ForumPage from "@/components/ForumPage.vue";
 import AdministratorPage from "@/components/AdministratorPage.vue";
 import PostPage from "@/components/PostPage.vue";
