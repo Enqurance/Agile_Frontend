55e97dc43419892a9205d21d50bb29c31112823f
Your Name <you@example.com>
2023-05-20 16:18:02 +0800

    [feat](postPage):增加删除、编辑接口

--- src/components/pc/PCPostPage.vue
+++ src/components/pc/PCPostPage.vue
@@ -12,8 +12,18 @@
                         <span class="post_title">{{ post.title }}</span>
                         <div class="post_tags">{{ post.tags }}</div>
                         <div class="post_stars">收藏数量：{{ post.stars }}</div>
+                        <el-button type="danger" @click="showDeletePost">删除post</el-button>
+                        <el-button @click="editPost">编辑post</el-button>
                     </div>
                     <div class="post_body">{{ post.body }}</div>
+                    <div>
+                        <el-dialog :title="testInfo" v-model="postDialogVisible">
+                            <div>
+                                <el-button @click="postDialogVisible = false">取消</el-button>
+                                <el-button type="primary" @click="submitEditForm">确认</el-button>
+                            </div>
+                        </el-dialog>
+                    </div>
 
                     <div class="post_footer">
                         <div class="avatar">
@@ -41,6 +51,7 @@
                             <div class="post_floor-header">{{ floor.user }} 发表于 {{ floor.time }}</div>
                             <div class="post_floor-body">{{ floor.body }}</div>
 
+                            <el-button type="danger" @click="showDeleteFloor(index)">删除floor</el-button>
                             <div v-if="floor.comments.length > 0">
                                 <div class="post_floor-comments-preview"
                                     v-for="(comment, index) in floor.comments.slice(0, 2)" :key="index">
@@ -54,6 +65,7 @@
                             </div>
                             <el-dialog v-model="floor.commentsDialogVisible" title="全部评论" width="50%">
                                 <div class="post_floor-comments" v-for="comment in floor.comments" :key="comment.id">
+                                    <el-button type="danger" @click="showDeleteComment(comment.id)">删除comment</el-button>
                                     <div class="post_floor-comment-header">{{ comment.user }} 评论于 {{ comment.time }}</div>
                                     <div class="post_floor-comment-body">{{ comment.body }}</div>
                                 </div>
@@ -96,6 +108,11 @@ export default {
                 tags: '标签1，标签2，标签3',
                 stars: 1,
                 body: "我是帖子的内容，我是帖子的内容，我是帖子的内容，我是帖子的内容，我是帖子的内容，我是帖子的内容，我是帖子的内容，"
+            },
+
+            postDialogVisible: false,
+            formPost: {
+
             },
 
             newFloorDialogVisible: false,
@@ -262,8 +279,89 @@ export default {
         addComment(index) {
             console.log("floor id" + this.floors[index].id)
             this.floors[index].commentsDialogVisible = false
-        }
+        },
 
+        showDeletePost() {
+            this.$confirm(
+                '确认删除该post？',
+                'Warning',
+                {
+                    confirmButtonText: '确认',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }
+            )
+                .then(() => {
+                    this.deletePost();
+                })
+                .catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消删除',
+                    });
+                });
+        },
+        deletePost() {// 执行删除post的逻辑
+            console.log('删除post');
+            this.$router.push({ path: '/Forum' })
+        },
+
+
+        showDeleteFloor(floorId) {
+            this.$confirm(
+                '确认删除该floor？',
+                'Warning',
+                {
+                    confirmButtonText: '确认',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }
+            )
+                .then(() => {
+                    this.deleteFloor(floorId);
+                })
+                .catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消删除',
+                    });
+                });
+        },
+        deleteFloor(floorId) {// 执行删除评论的逻辑
+            console.log('删除floor ID为' + floorId);
+        },
+
+        showDeleteComment(commentId) {
+            this.$confirm(
+                '确认删除该comment？',
+                'Warning',
+                {
+                    confirmButtonText: '确认',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }
+            )
+                .then(() => {
+                    this.deleteComment(commentId);
+                })
+                .catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '取消删除',
+                    });
+                });
+        },
+        deleteComment(commentId) {// 执行删除评论的逻辑
+            console.log('删除评论ID为' + commentId);
+        },
+
+        editPost() {
+            this.formPost = Object.assign({}, this.post)
+            this.postDialogVisible = true
+        },
+        submitEditForm() {
+            
+        },
     },
 
     mounted() {
