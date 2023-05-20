c0d57219777b294879223e0d221a1a5c01288d51
wzy2001wzy <864294516@qq.com>
2023-05-20 15:33:52 +0800

    [feat](myMessage_Mobile):new

--- src/components/InforPage.vue
+++ src/components/InforPage.vue
@@ -1,15 +1,12 @@
 <template>
     <PCInfoPage v-if="this.$device==='pc'"/>
-    <MyPost_Mobile v-if="this.$device!=='pc'"></MyPost_Mobile>
 </template>
 
 <script>
 import PCInfoPage from "@/components/pc/PCInforPage.vue";
-import MyPost_Mobile from "@/components/sub_components/infopage_sub_components/MyPost_Mobile";
-
 export default {
     name: "InforPage",
-    components: {MyPost_Mobile, PCInfoPage}
+    components: {PCInfoPage}
 }
 </script>
 
--- src/components/sub_components/infopage_sub_components/MyMessage_Mobile.vue
+++ src/components/sub_components/infopage_sub_components/MyMessage_Mobile.vue
@@ -1,87 +1,91 @@
 <template>
   <div
-      style="padding-left: 10%;padding-top:5%;padding-bottom:9%;background: rgb(246,246,246);width: 90%;height: 90%">
-    <div style="padding-left: 5%;font-size: 20px;width:400px;display: inline-block">
-      <div style="height: 80px">
-        我收到的
+      style="padding-left: 10%;padding-top:10%;background: rgb(246,246,246);width: 90%">
+    <div style="width: 90%">
+      <div style="margin-bottom: 3%;font-size: 20px;">
+        我的回复
+        <!--      <el-button style="margin-left: 40%" size="large" type="primary">-->
+        <!--        创建-->
+        <!--      </el-button>-->
       </div>
-
-      <ul class="infinite-list" style="overflow: auto;display: inline-block">
-        <div v-if="revs.length === 0">
-          <el-empty description="暂时还没有消息"/>
-        </div>
-        <div v-for="rev in revs" :key="rev.id"
-             style="border-radius: 20px; background: white; border: 2px solid rgb(246,246,246); width: 360px;height: 120px ">
-          <div v-if="rev.read===false" style="float: right;padding-top: 50px">
-            <el-icon>
-              <Warning/>
-            </el-icon>
+      <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+      >
+        <el-menu-item index="1" @click="Index='1'">我收到的</el-menu-item>
+        <div class="flex-grow"/>
+        <el-menu-item index="2" @click="Index='2'">我发起的</el-menu-item>
+      </el-menu>
+      <div v-if="this.Index==='1'">
+        <ul class="infinite-list" style="overflow: auto;display: inline-block">
+          <div v-if="revs.length === 0">
+            <el-empty description="暂时还没有消息"/>
           </div>
+          <div v-for="rev in revs" :key="rev.id"
+               style="border-radius: 20px; background: white; border: 2px solid rgb(246,246,246); width: 360px;height: 120px ">
+            <div v-if="rev.read===false" style="float: right;padding-top: 50px">
+              <el-icon>
+                <Warning/>
+              </el-icon>
+            </div>
 
-          <h3 style="padding: 0 20px;font-size:20px;height:50px;width:300px;overflow: hidden;"
-              @click="clickMsg(rev)">
-            {{ rev.title }}
-          </h3>
-          <p style="padding: 0 20px;font-size:16px;height:20px;width:300px;overflow: hidden;">
-            {{ rev.time }}
-          </p>
-          <el-dialog v-model="rev.detail" title="消息详情" width="30%" center>
+            <h3 style="padding: 0 20px;font-size:20px;height:50px;width:300px;overflow: hidden;"
+                @click="clickMsg(rev)">
+              {{ rev.title }}
+            </h3>
+            <p style="padding: 0 20px;font-size:16px;height:20px;width:300px;overflow: hidden;">
+              {{ rev.time }}
+            </p>
+            <el-dialog v-model="rev.detail" title="消息详情" width="80%" center>
             <span style="text-align: center">
               {{ rev.content }}
             </span>
 
-            <template #footer>
+              <template #footer>
               <span class="dialog-footer">
                 <el-button @click="rev.detail = false">关闭</el-button>
               </span>
-            </template>
-          </el-dialog>
-        </div>
-      </ul>
-
-
-    </div>
-    <div style="padding-left: 90px;font-size: 20px;width:400px;display: inline-block">
-      <div style="height: 80px">
-        我发起的
-      </div>
-
-      <ul class="infinite-list" style="overflow: auto;display: inline-block">
-        <div v-if="snds.length === 0">
-          <el-empty description="暂时还没有消息"/>
-        </div>
-        <div v-for="snd in snds" :key="snd.id"
-             style="border-radius: 20px; background: white; border: 2px solid rgb(246,246,246); width: 360px;height: 120px ">
-          <div v-if="snd.read===false" style="float: right;padding-top: 50px">
-            <el-icon>
-              <Warning/>
-            </el-icon>
+              </template>
+            </el-dialog>
           </div>
+        </ul>
+      </div>
+      <div v-if="this.Index==='2'">
+        <ul class="infinite-list" style="overflow: auto;display: inline-block">
+          <div v-if="snds.length === 0">
+            <el-empty description="暂时还没有消息"/>
+          </div>
+          <div v-for="snd in snds" :key="snd.id"
+               style="border-radius: 20px; background: white; border: 2px solid rgb(246,246,246); width: 360px;height: 120px ">
+            <div v-if="snd.read===false" style="float: right;padding-top: 50px">
+              <el-icon>
+                <Warning/>
+              </el-icon>
+            </div>
 
-          <h3 style="padding: 0 20px;font-size:20px;height:50px;width:300px;overflow: hidden;"
-              @click="clickMsg(snd)">
-            {{ snd.title }}
-          </h3>
-          <p style="padding: 0 20px;font-size:16px;height:20px;width:300px;overflow: hidden;">
-            {{ snd.time }}
-          </p>
-          <el-dialog v-model="snd.detail" title="消息详情" width="30%" center>
+            <h3 style="padding: 0 20px;font-size:20px;height:50px;width:300px;overflow: hidden;"
+                @click="clickMsg(snd)">
+              {{ snd.title }}
+            </h3>
+            <p style="padding: 0 20px;font-size:16px;height:20px;width:300px;overflow: hidden;">
+              {{ snd.time }}
+            </p>
+            <el-dialog v-model="snd.detail" title="消息详情" width="80%" center>
             <span style="text-align: center">
               {{ snd.content }}
             </span>
 
-            <template #footer>
+              <template #footer>
               <span class="dialog-footer">
                 <el-button @click="snd.detail = false">关闭</el-button>
               </span>
-            </template>
-          </el-dialog>
-        </div>
-      </ul>
-
-
+              </template>
+            </el-dialog>
+          </div>
+        </ul>
+      </div>
     </div>
-
   </div>
 
 </template>
@@ -91,10 +95,12 @@
 import {Warning} from "@element-plus/icons-vue";
 
 export default {
-  name: "MyMessage",
+  name: "MyMessage_Mobile",
   components: {Warning},
   data() {
     return {
+      Index: 0,
+      activeIndex: '1',
       revs: [
         {
           id: 123,
