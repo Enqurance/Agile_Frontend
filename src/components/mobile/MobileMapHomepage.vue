7817dfe48521832b17e406adaebca4d3f6bff326
xk <20373398@buaa.edu.cn>
2023-05-25 19:49:24 +0800

    [feat](Map): position

--- src/components/mobile/MobileMapHomepage.vue
+++ src/components/mobile/MobileMapHomepage.vue
@@ -186,13 +186,15 @@ export default {
                     let geolocation = new AMap.Geolocation({
                         enableHighAccuracy: true,//是否使用高精度定位，默认:true
                         maximumAge: 0,           //定位结果缓存0毫秒，默认：0
-                        convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+                        convert: false,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
                         showButton: true,        //显示定位按钮，默认：true
                         buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
                         buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
                         showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
                         showCircle: false,        //定位成功后用圆圈表示定位精度范围，默认：true
                         panToLocation: false,     //定位成功后将定位到的位置作为地图中心点，默认：true
+                        GeoLocationFirst: true,
+
                     })
 
                     this.map.addControl(geolocation)
@@ -209,13 +211,14 @@ export default {
 
                     function onComplete (data) {
                         // data是具体的定位信息
-                        let gpsPoint = GPS.gcj_encrypt(data.position.getLat(), data.position.getLng())
-                        console.log(gpsPoint)
-                        let lng= gpsPoint.lon;
-                        let lat= gpsPoint.lat;
+                        // let gpsPoint = GPS.gcj_encrypt(data.position.getLat(), data.position.getLng())
+                        // console.log(gpsPoint)
+                        // let lng= gpsPoint.lon;
+                        // let lat= gpsPoint.lat;
 
+                        geolocation._marker.setPosition([data.position.getLng(), data.position.getLat()])
                         geolocation._marker.setPosition([lng, lat])
-                        that.map.setCenter([lng, lat])
+                        // that.map.setCenter([lng, lat])
 
                         that.$message({
                             message: '定位成功lnglat: ' + lng + ', ' + lat,
