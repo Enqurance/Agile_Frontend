42dc12a6fcca834fb298b37d14081ff889236d55
xk <20373398@buaa.edu.cn>
2023-05-26 15:08:31 +0800

    [chore](PCPost): css

--- src/components/pc/PCInforPage.vue
+++ src/components/pc/PCInforPage.vue
@@ -4,7 +4,6 @@ import MyInfo from '@/components/pc/sub_components/infopage_sub_components/MyInf
 import MyComment from "@/components/pc/sub_components/infopage_sub_components/MyComment.vue";
 import MyMessage from "@/components/pc/sub_components/infopage_sub_components/MyMessage.vue";
 import MyPost from "@/components/pc/sub_components/infopage_sub_components/MyPost.vue";
-import { ref } from 'vue'
 import {
     User,
     Reading,
@@ -20,14 +19,22 @@ export default {
     },
     data() {
         return {
-            isCollapse: false
+            isCollapse: false,
+            Index: '1'
         }
     },
-    setup(){
-        let Index = ref('1');
-        return { Index }
+    computed: {
+        sub_menu() {
+            if (this.Index.length === 3) {
+                return parseInt(this.Index.at(2))
+            }
+            else {
+                return parseInt(this.Index)
+            }
+        }
     },
     mounted() {
+        this.Index = '1'
         window.addEventListener('resize', this.handleResize)
         this.handleResize()
     },
@@ -36,6 +43,11 @@ export default {
             this.isCollapse = window.innerWidth < 768 // 在窗口宽度小于 768px 时折叠菜单
         }
     },
+    watch: {
+        sub_menu(newData) {
+            console.log(newData)
+        }
+    }
 }
 </script>
 
@@ -47,32 +59,56 @@ export default {
             </el-header>
             <el-container style="height: 80%">
                 <el-aside width="15%">
-                    <el-menu :default-active="Index" :collapse="isCollapse" style="height: 100%">
+                    <el-menu default-active="1" :default-openeds="[]" :collapse="isCollapse" style="height: 100%; padding-top: 5%">
                         <el-menu-item index="1" @click="Index='1'">
                             <el-icon><User /></el-icon>
                             <template #title>
                                 <span>个人信息</span>
                             </template>
                         </el-menu-item>
-                        <el-menu-item index="2" @click="Index='2'">
-                            <el-icon><Reading /></el-icon>
-                            <template #title>我的帖子</template>
-                        </el-menu-item>
-                        <el-menu-item index="3" @click="Index='3'">
-                            <el-icon><ChatLineSquare /></el-icon>
-                            <template #title>我的回复</template>
-                        </el-menu-item>
-                        <el-menu-item index="4" @click="Index='4'">
-                            <el-icon><ChatRound /></el-icon>
-                            <template #title>我的消息</template>
-                        </el-menu-item>
+                        <el-sub-menu index="2">
+                            <template #title>
+                                <el-icon>
+                                    <Reading />
+                                </el-icon>
+                                <span>我的帖子</span>
+                            </template>
+                            <el-menu-item-group>
+                                <el-menu-item index="2-1" @click="Index='2-1'">所有帖子</el-menu-item>
+                                <el-menu-item index="2-2" @click="Index='2-2'">待整改帖子</el-menu-item>
+                            </el-menu-item-group>
+                        </el-sub-menu>
+                        <el-sub-menu index="3">
+                            <template #title>
+                                <el-icon>
+                                    <ChatLineSquare />
+                                </el-icon>
+                                <span>我的回复</span>
+                            </template>
+                            <el-menu-item-group>
+                                <el-menu-item index="3-1" @click="Index='3-1'">我的楼层</el-menu-item>
+                                <el-menu-item index="3-2" @click="Index='3-2'">我的评论</el-menu-item>
+                            </el-menu-item-group>
+                        </el-sub-menu>
+                        <el-sub-menu index="4">
+                            <template #title>
+                                <el-icon>
+                                    <ChatRound />
+                                </el-icon>
+                                <span>我的消息</span>
+                            </template>
+                            <el-menu-item-group>
+                                <el-menu-item index="4-1" @click="Index='4-1'">我收到的</el-menu-item>
+                                <el-menu-item index="4-2" @click="Index='4-2'">我发起的</el-menu-item>
+                            </el-menu-item-group>
+                        </el-sub-menu>
                     </el-menu>
                 </el-aside>
             <el-main style="height: 100%">
-                <div v-if="this.Index === '1'"><MyInfo></MyInfo></div>
-                <div v-if="this.Index === '2'"><MyPost /></div>
-                <div v-if="this.Index === '3'"><MyComment /></div>
-                <div v-if="this.Index === '4'"><MyMessage /></div>
+                <div v-if="this.Index.at(0) === '1'"><MyInfo></MyInfo></div>
+                <div v-if="this.Index.at(0) === '2'"><MyPost :sub-menu="sub_menu" /></div>
+                <div v-if="this.Index.at(0) === '3'"><MyComment :sub-menu="sub_menu" /></div>
+                <div v-if="this.Index.at(0) === '4'"><MyMessage :sub-menu="sub_menu" /></div>
             </el-main>
         </el-container>
         </el-container>
