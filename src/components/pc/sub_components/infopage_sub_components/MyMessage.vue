f3cfb6e0ad33c815416fa593a0f114594e048c75
wzy2001wzy <864294516@qq.com>
2023-05-28 21:15:00 +0800

    [feat]():msg_all

--- src/components/pc/sub_components/infopage_sub_components/MyMessage.vue
+++ src/components/pc/sub_components/infopage_sub_components/MyMessage.vue
@@ -1,6 +1,22 @@
 <template>
+
     <div style="margin: 0 auto;padding: 0 3%; width: 80%">
-        <div class="sub_div" v-if="subMenu === 1">
+        <div class="sub_div"  v-if="subMenu === 1">
+          <el-button type="primary" @click="readAllRev()"> 全部已读</el-button>
+          <el-button type="danger" @click="deleteRevDialog=true"> 全部删除</el-button>
+          <el-dialog v-model="deleteRevDialog" width="30%" center>
+            <div style="; text-align: center">
+              <el-text tag="b" style="color: black; font-size: 15px">
+                您确定删除所有消息吗？
+              </el-text>
+            </div>
+            <template #footer>
+              <div class="dialog-footer">
+                <el-button style="margin-left: 50%" @click="deleteRevDialog = false">取消</el-button>
+                <el-button type="primary" @click="delAllRev()">确认</el-button>
+              </div>
+            </template>
+          </el-dialog>
             <div style="overflow: auto;">
                 <el-empty v-if="revs.length === 0" class="empty_div" description="暂时还没有消息"/>
                 <div v-else class="post_div" v-for="rev in revs" :key="rev.id" @click="readMsg(rev)">
@@ -43,6 +59,21 @@
         </div>
 
         <div class="sub_div" v-if="subMenu === 2">
+          <el-button type="primary" @click="readAllSnd()"> 全部已读</el-button>
+          <el-button type="danger" @click="deleteSndDialog=true"> 全部删除</el-button>
+          <el-dialog v-model="deleteSndDialog" width="30%" center>
+            <div style="; text-align: center">
+              <el-text tag="b" style="color: black; font-size: 15px">
+                您确定删除所有消息吗？
+              </el-text>
+            </div>
+            <template #footer>
+              <div class="dialog-footer">
+                <el-button style="margin-left: 50%" @click="deleteSndDialog = false">取消</el-button>
+                <el-button type="primary" @click="delAllSnd()">确认</el-button>
+              </div>
+            </template>
+          </el-dialog>
             <div style="overflow: auto">
                 <div v-if="snds.length === 0">
                     <el-empty class="empty_div" description="暂时还没有消息"/>
@@ -105,6 +136,8 @@ export default {
         return {
             revs: [],
             snds: [],
+            deleteRevDialog:false,
+            deleteSndDialog:false
         }
     },
     methods: {
@@ -162,14 +195,16 @@ export default {
             }).catch((res) => console.log(res))
         },
         queryAllRev() {
+
             let that = this
+            console.log(that.$cookies.get('user_token'))
             that.$axios.get('/InfoPage/MyMessage/getReceiveMessage', {
                 headers: {
                     'token': that.$cookies.get('user_token')
                 }
 
             }).then((res) => {
-                console.log(res)
+                // console.log(res)
                 if (res.data.code === 200) {
                     // console.log("get rev success")
                     this.revs = res.data.data
@@ -190,7 +225,7 @@ export default {
                 }
 
             }).then((res) => {
-                console.log(res)
+                // console.log(res)
                 if (res.data.code === 200) {
                     // console.log("get snd success")
                     this.snds = res.data.data
@@ -202,6 +237,104 @@ export default {
                     })
                 }
             })
+        },
+        readAllRev(){
+          let that = this
+          // console.log(that.$cookies.get('user_token'));
+          that.$axios.post('/InfoPage/MyMessage/readAllReceiveMessage',{}, {
+            headers: {
+              'token': that.$cookies.get('user_token')
+            }
+          }).then((res) => {
+            console.log(res)
+            if (res.data.code === 200) {
+              this.$message({
+                type: 'info',
+                message: '操作成功',
+              });
+            }
+            else {
+              this.$message({
+                message: res.data.message,
+                type: 'error'
+              })
+            }
+          }).catch((res) => console.log(res))
+          this.queryAllRev()
+        },
+        delAllRev(){
+          let that = this
+          that.deleteRevDialog=false;
+          // console.log(msg);
+          that.$axios.post('/InfoPage/MyMessage/deleteAllReceiveMessage', {},{
+            headers: {
+              'token': that.$cookies.get('user_token')
+            }
+          }).then((res) => {
+            if (res.data.code === 200) {
+              // console.log("删除的帖子id为：" + msg.id)
+              this.$message({
+                type: 'info',
+                message: '操作成功',
+              });
+              this.revs=[];
+            }
+            else {
+              this.$message({
+                message: res.data.message,
+                type: 'error'
+              })
+            }
+          }).catch((res) => console.log(res))
+        },
+        readAllSnd(){
+          let that = this
+          // console.log(msg);
+          that.$axios.post('/InfoPage/MyMessage/readAllSendMessage', {},{
+            headers: {
+              'token': that.$cookies.get('user_token')
+            }
+          }).then((res) => {
+            if (res.data.code === 200) {
+              // console.log("删除的帖子id为：" + msg.id)
+              this.$message({
+                type: 'info',
+                message: '操作成功',
+              });
+            }
+            else {
+              this.$message({
+                message: res.data.message,
+                type: 'error'
+              })
+            }
+          }).catch((res) => console.log(res))
+          this.queryAllSnd();
+        },
+        delAllSnd(){
+          let that = this
+          that.deleteSndDialog=false;
+          // console.log(msg);
+          that.$axios.post('/InfoPage/MyMessage/deleteAllSendMessage',{}, {
+            headers: {
+              'token': that.$cookies.get('user_token')
+            }
+          }).then((res) => {
+            if (res.data.code === 200) {
+              // console.log("删除的帖子id为：" + msg.id)
+              this.$message({
+                type: 'info',
+                message: '操作成功',
+              });
+              this.snds=[];
+            }
+            else {
+              this.$message({
+                message: res.data.message,
+                type: 'error'
+              })
+            }
+          }).catch((res) => console.log(res))
         },
     },
     mounted() {
