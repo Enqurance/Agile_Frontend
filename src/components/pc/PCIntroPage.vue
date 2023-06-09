cfd3fcbbb02f70bf36afecc31ec449d740b52fb7
pangrj <pangrj@buaa.edu.cn>
2023-06-09 22:54:19 +0800

    [chore](Intro):美化了基本信息介面

--- src/components/pc/PCIntroPage.vue
+++ src/components/pc/PCIntroPage.vue
@@ -3,7 +3,7 @@ import PageHeader from './PCPageHeader.vue'
 import CopyrightICP from "@/components/CopyrightICP.vue";
 
 import {
-  User,
+  User, Star, 
   Reading,
 } from '@element-plus/icons-vue'
 
@@ -11,7 +11,7 @@ export default {
     name: "PCIntroPage",
     components: {
         CopyrightICP, PageHeader,
-        User, Reading, 
+        User, Reading, Star, 
     },
     data() {
         return {
@@ -31,7 +31,6 @@ export default {
 
 <template>
     <div style="height: 100%">
-        {{ this.Index }}
         <el-container style="height: 100%">
             <el-header style="padding-left: 0;padding-right: 0">
                 <PageHeader/>
@@ -49,69 +48,211 @@ export default {
                                 <span>基本信息</span>
                             </template>
                         </el-menu-item>
-                        <el-sub-menu index="2">
+                        <el-menu-item index="2" @click="Index='2'">
+                            <el-icon><Star /></el-icon>
+                            <template #title>
+                                <span>功能介紹</span>
+                            </template>
+                        </el-menu-item>
+                        <el-sub-menu index="3">
                             <template #title>
                                 <el-icon><User /></el-icon>
                                 <span>成员介绍</span>
                             </template>
-                            <el-menu-item index="2-1" @click="Index='2-1'">PM</el-menu-item>
-                            <el-menu-item index="2-2" @click="Index='2-2'">前端</el-menu-item>
-                            <el-menu-item index="2-3" @click="Index='2-3'">后端</el-menu-item>
+                            <el-menu-item index="3-1" @click="Index='3-1'">PM</el-menu-item>
+                            <el-menu-item index="3-2" @click="Index='3-2'">前端</el-menu-item>
+                            <el-menu-item index="3-3" @click="Index='3-3'">后端</el-menu-item>
                         </el-sub-menu>
                     </el-menu>
                 </el-aside>
-                <el-main height="100%">
+                <el-main height="100%" style="padding: 0px 0px 0px 0px;">
+                    <!--基本信息部分 可直接講div縮起-->
                     <div v-if="this.Index.at(0) === '1'" style="height: 100%">
                         <el-container style="height: 100%;">
-                        <el-header height="30%" style="align-self: center; padding-top: 5%; padding-bottom: 8%;">
-                            <p class="title">BUAAMapForum</p>
+                        <el-header height="60%" class="header">
+                            <view class="bg"><image class='back' src='@/assets/back.png'/></view>
+                            <view class="up">
+                                <p class="title">BUAAMapForum</p>
+                                <p class="detail">from Group Agile-埃杰</p>
+                            </view>
                         </el-header>
-                        <el-card style="margin-left: 8%; margin-right: 8%;">
-                        <el-container height="100%">
-                            <el-aside width="50%" style="border-right-width: thin;border-right-style: double;border-right-color: gainsboro;">
-                                    <div class="logo-container">
-                                        <img src="/img/logo.png" alt="logo" style="width: 100%; height: auto;">
-                                    </div>
-                            </el-aside>
-                            <el-main>
-                                    <div class="logo-container">
-                                        <p class="content">这里是Agile-埃杰软工小组,
+                        <el-card style="margin: 0%; height: 80%; padding-top: 5%;">
+                            <div class="logo-container">
+                                <p class="content">这里是Agile-埃杰软工小组,
                                             在应用中大家可以以Pin的形式浏览北航校园，也可以在论坛中进行咨询/分享，我们希望通过本应用为大家的大学生活提供便捷。</p>
-                                    </div>
-                            </el-main>
-                        </el-container>
+                            </div>
                         </el-card>
                         </el-container>
                     </div>
-                    <div v-if="this.Index === '2-1'">
-                        <el-row :gutter="20">
-                            <el-col :span="8"><div>
+                    <!--功能介紹部分-->
+                    <div v-if="this.Index.at(0) === '2'" style="height: 100%">
+                    </div>
+                    <!--成員信息部分-->
+                    <div v-if="this.Index === '3-1'" style="height: 100%;">
+                        <el-row height="10%" style="padding: 2% 0px 0px 2%;  margin-bottom: 0px;">
+                            <p style="padding-bottom: 0px; margin-bottom: 0px; font-size: 40px;">PM</p>
+                        </el-row>
+                        <el-row height="10%" style="padding: 2% 0px 2% 2%; border-bottom-width: thin;border-bottom-style: double;border-bottom-color: gainsboro;">
+                            <p style=" font-size: 17px; padding-left: 2%;">PM是团队的灵魂！</p>
+                        </el-row>
+                        <el-row :gutter="20" style="padding: 2% 0px 0px 3%;">
+                            <el-col :span="6"><div>
                                 <el-card>
                                     <template #header>
-                                    <div class="card-header">
-                                        <span>林子杰</span>
-                                    </div>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>林子杰</span>
+                                            </el-cow>
+                                        </el-row>
                                     </template>
                                 </el-card>
                             </div></el-col>
                         </el-row>
                     </div>
-                    <div v-if="this.Index.at(0) === '2-2'">
-                        <el-row :gutter="20">
-                            <el-col :span="8"><div></div></el-col>
-                            <el-col :span="8"><div></div></el-col>
-                            <el-col :span="8"><div></div></el-col>
+                    <div v-if="this.Index === '3-2'" style="height: 100%;">
+                        <el-row height="10%" style="padding: 2% 0px 0px 2%;  margin-bottom: 0px;">
+                            <p style="padding-bottom: 0px; margin-bottom: 0px; font-size: 40px;">FrontEnd</p>
+                        </el-row>
+                        <el-row height="10%" style="padding: 2% 0px 2% 2%; border-bottom-width: thin;border-bottom-style: double;border-bottom-color: gainsboro;">
+                            <p style=" font-size: 17px; padding-left: 2%;">前端是团队的窗口门户！</p>
+                        </el-row>
+                        <el-row :gutter="20" style="padding: 2% 0px 0px 3%;">
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>徐凯</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>庞睿加</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>秦铭悦</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>吳湛宇</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
                         </el-row>
                     </div>
-                    <div v-if="this.Index.at(0) === '2-3'">
-                        <el-row :gutter="20">
-                            <el-col :span="8"><div></div></el-col>
-                            <el-col :span="8"><div></div></el-col>
-                            <el-col :span="8"><div></div></el-col>
+                    <div v-if="this.Index === '3-3'" style="height: 100%;">
+                        <el-row height="10%" style="padding: 2% 0px 0px 2%;  margin-bottom: 0px;">
+                            <p style="padding-bottom: 0px; margin-bottom: 0px; font-size: 40px;">BackEnd</p>
+                        </el-row>
+                        <el-row height="10%" style="padding: 2% 0px 2% 2%; border-bottom-width: thin;border-bottom-style: double;border-bottom-color: gainsboro;">
+                            <p style=" font-size: 17px; padding-left: 2%;">后端是团队的坚实支撑！</p>
+                        </el-row>
+                        <el-row :gutter="20" style="padding: 2% 0px 0px 3%;">
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>李治圻</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
+                            <el-col :span="6"><div>
+                                <el-card>
+                                    <template #header>
+                                        <el-row :gutter="20" style="width: 100%; margin: 0px 0px 0px 0px;">
+                                            <el-cow :span="12">
+                                            <el-avatar
+                                                :size="40"
+                                                shape="circle"
+                                                :src="this.imageUrl"
+                                                style="user-select: none;">
+                                            </el-avatar>
+                                            </el-cow>
+                                            <el-cow :span="12" style=" padding-left: 5%; padding-top: 5%">
+                                            <span>杜维康</span>
+                                            </el-cow>
+                                        </el-row>
+                                    </template>
+                                </el-card>
+                            </div></el-col>
+                            <el-col :span="8"><div>
+                            </div></el-col>
                         </el-row>
                     </div>
-                    <div v-if="this.Index.at(0) === '3'">333</div>
-                    <div v-if="this.Index.at(0) === '4'">444</div>
                 </el-main>
             </el-container>
             <CopyrightICP></CopyrightICP>
@@ -123,7 +264,15 @@ export default {
 .title{
     font-family: fantasy;
     font-size: 3.2em;
-    color: #1f82ce;
+    color: #164d78;
+    text-align: center;
+    padding-top: 10%;
+}
+.detail{
+    font-family: fantasy;
+    font-size: 2.0em;
+    color: #164d78;
+    text-align: center;
 }
 .logo-container {
     margin-left: 20%;
@@ -135,8 +284,25 @@ export default {
     text-align: center;
 }
 .content{
-    padding-top: 5%;
-    font-family: fantasy;
-    font-size: 1.3em;
+    padding-top: 3%;
+    font-size: 1.2em;
+}
+.header{
+    width: 100%;
+    align-self: center;
+    padding: 0px 0px 0px 0px;
+}
+.up{
+    width: 85%;
+    height: 60%;
+    position: relative;
+}
+.bg{
+    width: 85%;
+    height: 60%;
+    position: absolute;
+    background: url("@/assets/back.png");
+    background-size: cover;
+    opacity: 0.5
 }
 </style>
